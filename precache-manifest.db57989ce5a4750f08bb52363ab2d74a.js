df52eb03eafa78aa201aa28836ba53c5522277f6
Sahil <51375919+sa1123@users.noreply.github.com>
2023-06-03 11:10:03 +0530

    Updates

---
+++ precache-manifest.db57989ce5a4750f08bb52363ab2d74a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "06bbee1ca196c9c0dd64",
+    "url": "/React-Calc-CN/static/css/main.4c72e792.chunk.css"
+  },
+  {
+    "revision": "06bbee1ca196c9c0dd64",
+    "url": "/React-Calc-CN/static/js/main.81bc24e8.chunk.js"
+  },
+  {
+    "revision": "39b08a78da708ac8638b",
+    "url": "/React-Calc-CN/static/js/runtime~main.f890eec0.js"
+  },
+  {
+    "revision": "b94cd5aada430b0d4459",
+    "url": "/React-Calc-CN/static/js/2.eb801ab1.chunk.js"
+  },
+  {
+    "revision": "15e0877ee0fad82e6a21e9c64fb67930",
+    "url": "/React-Calc-CN/index.html"
+  }
+];
